017630b6b7fe1723e042c3c39f1e590f740e51f1
Peter Yoachim <yoachim@uw.edu>
2022-08-22 15:39:53 -0700

    making baseline

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # survey_predicted
 Run a Rubin survey simulation, then also generate a 15 dat forecast of potential observations for each night
+
+
+Started with the survey from https://github.com/lsst-sims/sims_featureScheduler_runs2.2/tree/main/baseline/baseline.py
+
+Warning! Survey simulation results are not cross-platform repeatable. So all steps should be run on the same hardware.
+
---
+++ generate_baseline.py
@@ -0,0 +1,26 @@
+from baseline import create_scheduler
+from rubin_sim.scheduler.schedulers import simple_filter_sched
+from rubin_sim.scheduler import sim_runner
+from rubin_sim.scheduler.modelObservatory import Model_observatory
+import numpy as np
+
+
+if __name__ == '__main__':
+    nside = 32
+    survey_length = 365.25
+    fileroot = 'baseline_'
+    extra_info = None
+    illum_limit = 40.
+    years = np.round(survey_length/365.25)
+    verbose = False
+
+    scheduler = create_scheduler()
+    n_visit_limit = None
+    filter_sched = simple_filter_sched(illum_limit=illum_limit)
+    observatory = Model_observatory(nside=nside)
+    observatory, scheduler, observations = sim_runner(observatory, scheduler,
+                                                      survey_length=survey_length,
+                                                      filename=fileroot+'%iyrs.db' % years,
+                                                      delete_past=True, n_visit_limit=n_visit_limit,
+                                                      verbose=verbose, extra_info=extra_info,
+                                                      filter_scheduler=filter_sched)
